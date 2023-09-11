7552eaace374f6f983b60207536e2f72253b1443
Holyangelmom <944505383@qq.com>
2023-09-11 12:55:20 +0800

    Create OperateType.java

---
+++ OperateType.java
@@ -0,0 +1,17 @@
+package com.design;
+
+
+/**
+ * 计算操作枚举
+ *
+ * @author likp2
+ * @department 营销云_新零售产品
+ * @date 2023/9/9
+ */
+public enum OperateType {
+
+    ADD,
+    SUB,
+    MULTI,
+    DIV
+}
